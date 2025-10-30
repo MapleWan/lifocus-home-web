ca784bbcfc228fa2f952c5eeaebd7e19c7a6da0b
MapleWan <1041709112@qq.com>
2025-10-30 23:39:26 +0800

    feat(SiderBar): 根据设备类型动态调整内容布局
    添加对移动设备的支持，当检测到非PC设备时，将内容区域调整为底部对齐并增加底部边距

--- src/components/layout/components/SiderBar.vue
+++ src/components/layout/components/SiderBar.vue
@@ -1,11 +1,19 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import useMainStore from '@/stores/main.js'
+const mainStore = useMainStore()
+const isPc = computed(() => mainStore.isPc)
+</script>
 
 <template>
   <div class="side-bar h-full flex flex-col justify-between">
     <div class="header">
       <slot name="header"> header </slot>
     </div>
-    <div class="content flex flex-col justify-start flex-1 m-t-4 gap-1">
+    <div
+      class="content flex flex-col flex-1 m-t-4 gap-1"
+      :class="{ 'justify-end': !isPc, 'm-b-8': !isPc, 'justify-start': isPc }"
+    >
       <slot name="content"> content </slot>
     </div>
     <div class="footer">
